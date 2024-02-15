be3a5ecb71e8815a4b2bb2c697c1f492406ed25f
abdullahkassem <abdullahkassem@aucegypt.edu>
2024-02-15 18:30:11 +0200

    Added some basic UI

--- script.js
+++ script.js
@@ -1,7 +1,43 @@
-const options = ["rock","paper","sissors"];
-let playerWins = 0;
-let compWins = 0;
-let Draws = 0;
+const options = ["rock","paper","scissor"];
+
+const winTracker = {
+    playerWins: 0,
+    compWins: 0,
+    draws: 0,
+  
+    playerWinsIncrement() {
+      this.playerWins++;
+      const score = document.querySelector("#playerWins .scoreNum");
+      score.textContent = this.playerWins;
+      if(this.playerWins >= 5)
+      {
+        console.log("Player Wins!!");
+      }
+    },
+  
+    compWinsIncrement() {
+      this.compWins++;
+      const score = document.querySelector("#computerWins .scoreNum");
+      score.textContent = this.compWins;
+    },
+  
+    drawIncrement() {
+      this.draws++;
+      const score = document.querySelector("#numOfDraws .scoreNum");
+      score.textContent = this.draws;
+    },
+
+    resetWins() {
+        this.playerWins = 0;
+        this.compWins = 0;
+        this.draws = 0;
+    },
+  };
+  
+
+// let playerWins = 0;
+// let compWins = 0;
+// let Draws = 0;
 
 function getComputerChoice()
 {
@@ -12,51 +48,67 @@ function getComputerChoice()
 function playRound(playerSelection, compSelection)
 {
     const playerIndex = options.indexOf(playerSelection);
+    if (playerIndex == -1) {
+        console.error("wrong player choice entered")
+    }
     const compIndex = options.indexOf(compSelection);
     const sub = playerIndex - compIndex;
 
     if(sub==-2 || sub==1)
     {
-        playerWins++;
-        return `You win, ${playerSelection} beats ${compSelection}`
+        winTracker.playerWinsIncrement();
+        console.log(`You win, ${playerSelection} beats ${compSelection}`);
+        return;
     }
     if(sub==2 || sub==-1)
     {
-        compWins++;
-        return `You Lose, ${compSelection} beats ${playerSelection}`
+        winTracker.compWinsIncrement();
+        console.log( `You Lose, ${compSelection} beats ${playerSelection}`);
+        return;
     }
-    Draws++;
-    return "Draw !!"
+    winTracker.drawIncrement();
+    console.log( "Draw !!");
+    
 }
 
-function getPlayerChoice()
+
+// function getPlayerChoice()
+// {
+//     let playerPlay;
+//     while(true)
+//     {
+//         playerPlay = prompt("Enter your Move.")
+//         if(options.includes(playerPlay))
+//             {break;}
+//         else
+//             {alert("Please enter a valid choice.");}
+//     }
+
+//     return playerPlay.toLowerCase();
+// }
+
+
+// function playGame()
+// {
+//     for (let index = 0; index < 5; index++) {
+//         const PlayerPlay = getPlayerChoice();
+//         const CompPlay = getComputerChoice();
+//         console.log(playRound(PlayerPlay,CompPlay));
+//     }
+//     if (playerWins > compWins)
+//         alert("Player has won in best of 5");
+//     else
+//         alert("Computer has won in best of 5");
+// }
+
+function optionPressed()
 {
-    let playerPlay;
-    while(true)
-    {
-        playerPlay = prompt("Enter your Move.")
-        if(options.includes(playerPlay))
-            {break;}
-        else
-            {alert("Please enter a valid choice.");}
-    }
-
-    return playerPlay.toLowerCase();
+    const clickedId = this.id;
+    console.log(`Element with ID: ${clickedId} was clicked.`);
+    playRound(clickedId, getComputerChoice() );
 }
 
-function playGame()
-{
-    for (let index = 0; index < 5; index++) {
-        const PlayerPlay = getPlayerChoice();
-        const CompPlay = getComputerChoice();
-        console.log(playRound(PlayerPlay,CompPlay));
-    }
-    if (playerWins > compWins)
-        alert("Player has won in best of 5");
-    else
-        alert("Computer has won in best of 5");
-}
-
-
-playGame();
-
+const buttons = document.querySelectorAll(".options");
+buttons.forEach( button => {
+    button.addEventListener("click",optionPressed)
+    });
